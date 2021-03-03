0b664bd9841b294b38231a63d2ab28d6b0ad5f91
tubakoc <tubakoc@users.noreply.github.com>
2021-03-03 21:09:33 +0300

    shoppingApp

---
+++ lib/controllers/productcontroller.dart
@@ -0,0 +1,27 @@
+import 'package:get/state_manager.dart';
+import 'package:shopping_app/models/product.dart';
+import 'package:shopping_app/services/remote_services.dart';
+
+
+class ProductController extends GetxController {
+  var isLoading = true.obs;
+  var productList = List<Product>().obs;
+
+  @override
+  void onInit() {
+    fetchProducts();
+    super.onInit();
+  }
+
+  void fetchProducts() async {
+    try {
+      isLoading(true);
+      var products = await RemoteServices.fetchProducts();
+      if (products != null) {
+        productList.value = products;
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
